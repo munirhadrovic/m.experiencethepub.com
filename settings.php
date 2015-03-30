4824b7bfbb0097b4122408f47b9bafd2b6ee857a
novicaa <novicamiketic@gmail.com>
2015-03-30 14:09:04 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="The Pub Perimeter 4400 Ashford Dunwoody Rd. Atlanta, GA 30346"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Atlanta, GA 30346";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=7705570883;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
